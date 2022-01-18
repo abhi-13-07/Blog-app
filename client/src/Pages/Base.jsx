6f69a8f3ae218b471ac32f0afa8df6cfbb02419a
abhi-13-07 <abhishek.13.july.02@gmail.com>
2022-01-18 20:00:50 +0530

    add navigations

--- client/src/Pages/Base.jsx
+++ client/src/Pages/Base.jsx
@@ -1,10 +1,29 @@
 import React from "react";
 import { Header } from "../Components";
+import { useNavigate } from "react-router-dom";
 
 const Base = ({ children }) => {
+	const navigate = useNavigate();
+
+	const handleLoginClick = () => {
+		navigate("/login");
+	};
+
+	const handleSignupClick = () => {
+		navigate("/signup");
+	};
+
+	const handleLogoutClick = () => {
+		navigate("/login");
+	};
+
 	return (
 		<div>
-			<Header />
+			<Header
+				onLoginClick={handleLoginClick}
+				onSignupClick={handleSignupClick}
+				onLogoutClick={handleLogoutClick}
+			/>
 			<div className="section">{children}</div>
 		</div>
 	);

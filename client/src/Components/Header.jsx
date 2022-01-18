d69a16f7d8eff4774fb4e698364a536514597ce5
abhi-13-07 <abhishek.13.july.02@gmail.com>
2022-01-18 19:55:54 +0530

    add click event handlers

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Button } from "./index";
 
-const Header = ({ user, width }) => {
+const Header = ({ user, width, onLoginClick, onSignupClick, onLogoutClick }) => {
 	return (
 		<header className="header">
 			<div className="brand-container">
@@ -11,11 +11,29 @@ const Header = ({ user, width }) => {
 			<div className="btn-container">
 				{!user ? (
 					<>
-						<Button label="Log-in" color="dark" btnStyle="outline" size="sm" />
-						<Button label="Sign-up" color="dark" btnStyle="contained" size="sm" />
+						<Button
+							label="Log-in"
+							color="dark"
+							btnStyle="outline"
+							size="sm"
+							onClick={onLoginClick}
+						/>
+						<Button
+							label="Sign-up"
+							color="dark"
+							btnStyle="contained"
+							size="sm"
+							onClick={onSignupClick}
+						/>
 					</>
 				) : (
-					<Button label="Logout" color="dark" btnStyle="outline" size="sm" />
+					<Button
+						label="Logout"
+						color="dark"
+						btnStyle="outline"
+						size="sm"
+						onClick={onLogoutClick}
+					/>
 				)}
 			</div>
 		</header>
@@ -25,6 +43,9 @@ const Header = ({ user, width }) => {
 Header.propTypes = {
 	user: PropTypes.object,
 	width: PropTypes.number,
+	onLoginClick: PropTypes.func,
+	onSignupClick: PropTypes.func,
+	onLogoutClick: PropTypes.func,
 };
 
 export default Header;

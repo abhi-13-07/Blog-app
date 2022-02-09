30741f538392bb706b2f4922215e68e4358b0a5b
abhi-13-07 <abhishek.13.july.02@gmail.com>
2022-02-09 12:03:51 +0530

    resolve conflicts

--- server/server/settings.py
+++ server/server/settings.py
@@ -166,12 +166,3 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-CORS_ALLOW_METHODS = [
-'DELETE',
-'GET',
-'OPTIONS',
-'PATCH',
-'POST',
-'PUT'
-]

28e7caaa092cb1f301f71d69450a67ae7145238a
L0K1E <logeshkeyboader@gmail.com>
2022-03-15 06:31:58 -0800

    markdown sanitization & slugfield modification

--- server/api/admin.py
+++ server/api/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
-from api.models import BlogPost
+from api.models import BlogPost, Like
 
-admin.site.register(BlogPost)
+class UserAdminConfig(admin.ModelAdmin):
+
+    search_fields = ('title', 'body')
+    ordering = ('-date_published',)
+    list_display = ('title', 'user','slug')
+
+admin.site.register(BlogPost, UserAdminConfig)
+admin.site.register(Like)
--- server/api/models.py
+++ server/api/models.py
@@ -1,9 +1,8 @@
-import email
+import hashlib, uuid
 from pyexpat import model
 from django.db import models
 from account.serializers import UserDetailsSerializer
 from rest_framework import serializers
-from autoslug import AutoSlugField
 from django.template.defaultfilters import slugify
 from django.conf import settings
 from account.models import User
@@ -14,13 +13,34 @@ class BlogPost(models.Model):
     body = models.TextField(max_length=10000, null=False, blank=False)
     date_published = models.DateTimeField(auto_now_add=True, verbose_name="date published")
     date_updated = models.DateTimeField(auto_now=True, verbose_name="data updated")
+    likes = models.ManyToManyField(User, default=None, blank=True, related_name='likes')
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=False)
     slug = models.SlugField(null=True, blank=True)
 
     def __str__(self):
         return self.title
 
+    def like_count(self):
+        return self.likes.all().count()
+
     def save(self, *args, **kwargs):
         if not self.id:
-            self.slug = slugify(self.title)
+            clean_string = self.title
+            salt = uuid.uuid4().hex
+            print(salt)
+            self.slug = int(hashlib.sha512(clean_string.encode('utf-8') + salt.encode('utf-8')).hexdigest(), 16) % (10 ** 8)
         super(BlogPost, self).save(*args, **kwargs)
+
+LIKE_CHOICES = (
+    ('Like', 'Like'),
+    ('Dislike', 'Dislike')
+
+)
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, )
+    post = models.ForeignKey(BlogPost,on_delete=models.CASCADE)
+    value = models.CharField(choices=LIKE_CHOICES, default='Like', max_length=10)
+
+    def __str__(self):
+        return str(self.post)
